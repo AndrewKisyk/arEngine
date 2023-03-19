3a1f150cbb14b81b1db4680701eba760c8c2a76a
“AndrewKisyk” <polyglot893@gmail.com>
2023-03-20 00:32:44 +0200

    base opencv project

---
+++ app/src/main/java/com/android/arengiene/MainActivity.java
@@ -0,0 +1,145 @@
+package com.android.arengiene;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Toast;
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.JavaCameraView;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Mat;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import com.android.arengiene.opencvnativeandroidstudio.R;
+
+public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
+
+    private static final String TAG = "OCVSample::Activity";
+    private CameraBridgeViewBase _cameraBridgeViewBase;
+    private Mat mRGBA;
+
+    private BaseLoaderCallback _baseLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case LoaderCallbackInterface.SUCCESS: {
+                    Log.i(TAG, "OpenCV loaded successfully");
+                    // Load ndk built module, as specified in moduleName in build.gradle
+                    // after opencv initialization
+                    System.loadLibrary("native-lib");
+                    _cameraBridgeViewBase.enableView();
+                }
+                break;
+                default: {
+                    super.onManagerConnected(status);
+                }
+            }
+        }
+    };
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION|
+                        View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        setContentView(R.layout.activity_main);
+
+        // Permissions for Android 6+
+        ActivityCompat.requestPermissions(MainActivity.this,
+                new String[]{Manifest.permission.CAMERA},
+                1);
+        _cameraBridgeViewBase = (CameraBridgeViewBase) findViewById(R.id.main_surface);
+        _cameraBridgeViewBase.setVisibility(SurfaceView.VISIBLE);
+        _cameraBridgeViewBase.setCvCameraViewListener(this);
+        JavaCameraView javaCameraView = findViewById(R.id.main_surface);
+        initializeCamera(javaCameraView, CameraBridgeViewBase.CAMERA_ID_BACK);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        disableCamera();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (!OpenCVLoader.initDebug()) {
+            //Log.d(TAG, "Internal OpenCV library not found. Using OpenCV Manager for initialization");
+            System.loadLibrary("opencv_java4");
+            //OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION, this, _baseLoaderCallback);
+        } else {
+            Log.d(TAG, "OpenCV library found inside package. Using it!");
+            _baseLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case 1: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Toast.makeText(MainActivity.this, "Permission denied to read your External storage", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
+    }
+
+    private void initializeCamera(JavaCameraView javaCameraView, int activeCamera){
+        javaCameraView.setCameraIndex(activeCamera);
+        javaCameraView.setVisibility(CameraBridgeViewBase.VISIBLE);
+        javaCameraView.setCvCameraViewListener(this);
+    }
+
+    public void onDestroy() {
+        super.onDestroy();
+        disableCamera();
+    }
+
+    public void disableCamera() {
+        if (_cameraBridgeViewBase != null)
+            _cameraBridgeViewBase.disableView();
+    }
+
+    public void onCameraViewStarted(int width, int height) {
+    }
+
+    public void onCameraViewStopped() {
+    }
+
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        mRGBA = inputFrame.rgba();
+        return mRGBA;
+    }
+
+    public native void salt(long matAddrGray, int nbrElem);
+}
+
